86beb4b200ed5b02cd734f5e03ec5085b3f19c63
sadik <sadikislam46@gmail.com>
2023-06-24 21:13:18 +0100

    convert image to rgb

--- Hand-Tracking/main.py
+++ Hand-Tracking/main.py
@@ -7,3 +7,16 @@ mp_hands = mp.solutions.hands
 
 # initialise video capture with webcam
 cap = cv2.VideoCapture(0)
+
+with mp_hands.hands(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+) as hands:
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("Failed to read video")
+            break
+
+        # convert image from bgr to rgb
+        image = cv2.cvtColor(frame, cv2.COLOR_GBR2RGB)
