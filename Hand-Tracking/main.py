94b9453cb10a00141b7b7206d8a21f40a2168738
sadik <sadikislam46@gmail.com>
2023-06-24 20:51:39 +0100

    a

--- Hand-Tracking/main.py
+++ Hand-Tracking/main.py
@@ -1,28 +1,9 @@
 import cv2
-import mediapipe as mp
-import time # to check the framerate
+import mediapipe as mediapipe
 
+# initialise hand tracking module
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# initialise video capture with webcam
 cap = cv2.VideoCapture(0)
-
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
-
-while True:
-    success, img = cap.read() # will give frame
-
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
-    results = hands.process(imgRGB)
-    # if hand is present, print coordinates of landmarks of hand
-    print(results.multi_hand_landmarks)
-
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
-    cv2.imshow("Image", img)
-    if cv2.waitKey(1) == ord('q'): #press q to exit
-        break
-
-cap.release()
-cv2.destroyAllWindows()
