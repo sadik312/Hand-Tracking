dfe7ae84ff56f2ffba7a26e562586ac529906d96
sadik312 <49173163+sadik312@users.noreply.github.com>
2023-07-27 23:03:59 +0100

    Update main.py

--- Hand-Tracking/main.py
+++ Hand-Tracking/main.py
@@ -1,20 +1,35 @@
 import cv2 
 import mediapipe as mp
-import time
 
-# Initialise webcam
-cap = cv2.VideoCapture(1)
+# Initialise hand tracking module from mediapipe
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+# Open webcam for capturing video
+cap = cv2.VideoCapture(2)
 
+# Create loop to read frames from webcam
 while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
+    ret, frame = cap.read()
+    if not ret:
+        break
+    # use mp_hands module to detect hands in each frame and track
+    with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        # convert img BGR to RGB
+        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(image_rgb)
+        
+        # If hands are detected, draw landmarks on hands
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                )
+                
+    # Display frame with hand landmarks
+    cv2.imshow('Hand Tracking', frame)
+    if cv2.waitKey(1) & 0xFF == 27: # 'Esc' to exit
+        exit
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img)
+cap.release()
+cv2.destroyAllWindows(
